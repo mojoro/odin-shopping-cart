366aab2833325168fd87a2e8832580243fdf01e7
John Moorman <45500240+mojoro@users.noreply.github.com>
2025-11-08 17:11:05 +0100

    Create navbar skeleton

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,20 @@
 import styles from "./NavBar.module.css";
+import { NavLink } from "react-router";
 
 const NavBar = () => {
-  return <nav className={styles.nav}>I'm a navBar!</nav>;
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/" className={styles.navLink}>
+        Home
+      </NavLink>
+      <NavLink to="/shop" className={styles.navLink}>
+        Shop
+      </NavLink>
+      <NavLink to="/cart" className={styles.navLink}>
+        Cart
+      </NavLink>
+    </nav>
+  );
 };
 
 export default NavBar;
